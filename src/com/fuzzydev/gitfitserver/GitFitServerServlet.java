677ee3a7d3c391e01f309891549501734deeea73
DejanRistic <dejanr23@gmail.com>
2012-07-13 10:57:12 -0400

    modified project files, deployed to app engine using old fuzzymessenger account, temporary to test with

--- src/com/fuzzydev/gitfitserver/GitFitServerServlet.java
+++ src/com/fuzzydev/gitfitserver/GitFitServerServlet.java
@@ -3,13 +3,13 @@ package com.fuzzydev.gitfitserver;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import org.omg.CORBA.portable.InputStream;
+
 @SuppressWarnings("serial")
 public class GitFitServerServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-		//comment merge
+		InputStream in = req.getInputStream();
+		in.read_string();
 	}
 	
 
